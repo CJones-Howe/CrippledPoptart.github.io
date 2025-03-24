5258587b0e505eb875e5687aba0ee4b335712d16
CrippledPoptart <zsmith-fox@albany.edu>
2025-03-24 19:56:12 -0400

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,14 @@
+// using let
+let name = "Poptart";
+let age = "Old";
+let fav_food = "Pizza";
+let isStudent = true;
+document.getElementById("demo1").innerHTML =
+	"Name: " + name + "<br>Age: " + age + "<br>Student: " + isStudent + "<br>Favorite Food: " + fav_food;
+
+var city = "Albany";
+let state = "NY";
+const country = "USA";
+
+document.getElementById("demo2").innerHTML =
+	"Location " + city + ", " + state + ", " + country;
