89156376bea5dd3998eefbd85a74e874115d8cd2
CrippledPoptart <zsmith-fox@albany.edu>
2025-03-24 19:29:31 -0400

    Add files via upload

---
+++ Output test.js
@@ -0,0 +1,15 @@
+function showInnerHTML() {
+    document.getElementById("output1").innerHTML = "Hello Poptarts";
+}
+
+function useDocumentWrite() {
+    document.write("This makes me feel fat.");
+}
+
+function showAlert() {
+    alert("HELP IM SCARED");
+}
+
+function showConsole() {
+    console.log("I tried to get this to work 3 times. ");
+}
